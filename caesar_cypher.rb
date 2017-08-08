6b65ca36263c9d8ecbbeff2b375bc027689c815a
Martyn <martynj2010@icloud.com>
2017-08-08 19:40:14 +0100

    First draft, need to add capitilization, spaces and wrapping z - a

--- caesar_cypher.rb
+++ caesar_cypher.rb
@@ -1,14 +1,21 @@
 def caesar_cipher (word, key)
 
-	alphabet = [a..z]
-	word.downcase
+	alphabet = ("a".."z").to_a
+	encrypted = []
+
+	word.downcase!
 	
-	encrypted.new(word.length){
-		word.each do |l|
-			index = alphabet.find_index(l)
-			index += key
-			alphabet[index]
-		end
-	}
-	puts encrypted.join
+	word.each_char do |l|
+		index = alphabet.find_index(l)
+		index += key
+		puts "Encryption for #{l} is: #{alphabet[index]}"
+		encrypted << alphabet[index]
+	end
+	puts "Caesar Cypher for #{word} is #{encrypted.join}"
 end
+
+ puts "Enter word to encrypt:"
+ w = gets.chomp
+ puts " Enter key (must be an integer)"
+ k = gets.chomp
+ caesar_cipher(w, k.to_i)
